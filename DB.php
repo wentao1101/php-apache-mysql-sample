d74598431caccfd0c0da7b24729e4e8f4e4dfd11
wentao1101 <wentao1101@gmail.com>
2015-11-26 13:54:14 +0800

    Update DB.php

--- DB.php
+++ DB.php
@@ -8,10 +8,10 @@ class DB
 
     function __construct()
     {
-        $serverName = env("MYSQL_PORT_3306_TCP_ADDR", "tcp://10.10.26.58:3306");
-        $databaseName = env("MYSQL_INSTANCE_NAME", "daocloud");
-        $username = env("MYSQL_USERNAME", "root");
-        $password = env("MYSQL_PASSWORD", "root");
+        $serverName = env("MYSQL_PORT_3306_TCP_ADDR", "localhost");
+        $databaseName = env("MYSQL_INSTANCE_NAME", "homestead");
+        $username = env("MYSQL_USERNAME", "homestead");
+        $password = env("MYSQL_PASSWORD", "secret");
 
         try {
             $this->pdo = new PDO("mysql:host=$serverName;dbname=$databaseName", $username, $password);
